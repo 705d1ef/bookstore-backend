d9cf8f5a25f81fbe3747834cdc997b8d899ef938
Fiodor <307605post@gmail.com>
2024-03-30 17:00:59 +0000

    remove comments

--- src/app/settings/base.py
+++ src/app/settings/base.py
@@ -11,7 +11,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-cz5*m(xgr2chr2n%!x#@r2#j(37as)7f-nf24$pid3kvrc32m!'
 
 env = environ.Env()
 environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
